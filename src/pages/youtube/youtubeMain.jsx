844cccea7355e9cc0acb3ee20793239ca9018efe
ddangju <duswn4338@gmail.com>
2021-11-25 20:40:11 +0900

    검색기능

--- src/pages/youtube/youtubeMain.jsx
+++ src/pages/youtube/youtubeMain.jsx
@@ -2,9 +2,40 @@ import React, { useState, useEffect } from "react";
 import { useHistory } from "react-router-dom";
 import "../../styles/youtubeMain.scss";
 import VideoList from "../youtube/videoList";
+import Nav from "../youtube/youtubeNav";
 
 const YoutubeMain = (props) => {
   const [video, setVideo] = useState([]);
+  let [name, setName] = useState();
+  const history = useHistory();
+
+  const search = (query) => {
+    const requestOptions = {
+      method: "GET",
+      redirect: "follow",
+    };
+
+    fetch(
+      `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=${query}&key=AIzaSyCtO1ZI8FSJl8PLT4d9vxh55y9-tjhdTeA&key=AIzaSyCtO1ZI8FSJl8PLT4d9vxh55y9-tjhdTeA`,
+      requestOptions
+    )
+      .then((response) => response.json())
+      .then((result) => setVideo(result.items))
+      .catch((error) => console.log("error", error));
+    // var requestOptions = {
+    //   method: "GET",
+    //   redirect: "follow",
+    // };
+
+    // fetch(
+    //   `https://www.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&q=${query}&key=AIzaSyCT4sWJm_bgXreVMubNOjQ5-MgwfQjJEhM&key=AIzaSyCT4sWJm_bgXreVMubNOjQ5-MgwfQjJEhM`,
+    //   requestOptions
+    // )
+    //   .then((response) => response.json())
+    //   // .then((result) => setVideo(result.items))
+    //   .catch((error) => console.log("error", error));
+  };
+
   useEffect(() => {
     const requestOptions = {
       method: "GET",
@@ -12,16 +43,13 @@ const YoutubeMain = (props) => {
     };
 
     fetch(
-      "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=20&key=AIzaSyCU6eEd0vE7rPsnuhwggrOsoKd7ACYB-hQ&key=AIzaSyCU6eEd0vE7rPsnuhwggrOsoKd7ACYB-hQ",
+      "https://www.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=20&key=AIzaSyCtO1ZI8FSJl8PLT4d9vxh55y9-tjhdTeA&key=AIzaSyCtO1ZI8FSJl8PLT4d9vxh55y9-tjhdTeA",
       requestOptions
     )
       .then((response) => response.json())
       .then((result) => setVideo(result.items))
       .catch((error) => console.log("error", error));
   }, []);
-
-  let [name, setName] = useState();
-  const history = useHistory();
 
   const inputValue = history.location.state;
 
@@ -35,7 +63,7 @@ const YoutubeMain = (props) => {
 
   return (
     <div>
-      <h4>{name}님, 안녕하세요</h4>
+      <Nav search={search} name={name}></Nav>
       <VideoList video={video} />
     </div>
   );
---
+++ src/pages/youtube/youtubeNav.jsx
@@ -0,0 +1,49 @@
+import React, { useRef } from "react";
+import "../../styles/youtubeNav.scss";
+import search from "../../images/search.png";
+import logo from "../../images/logo.png";
+
+const YoutubeNav = (props) => {
+  let inputRef = useRef();
+
+  const handleSearch = () => {
+    const value = inputRef.current.value;
+    props.search(value);
+  };
+
+  const handleClick = () => {
+    handleSearch();
+  };
+  const handlePress = (event) => {
+    if (event.key === "Enter") {
+      handleSearch();
+    }
+  };
+  return (
+    <div className="navContainer_youtube">
+      <div className="logoContainer">
+        <img src={logo} alt="logo"></img>
+        <p className="logo_youtube">YouTube</p>
+      </div>
+      <div className="searchContainer">
+        <input
+          ref={inputRef}
+          placeholder="search"
+          className="search"
+          onKeyPress={handlePress}
+          // onChange={handleSearch}
+        />
+        <img
+          className="searchImg"
+          src={search}
+          alt="검색"
+          onClick={handleClick}
+        />
+      </div>
+
+      <p className="userName">{props.name}님, 안녕하세요!</p>
+    </div>
+  );
+};
+
+export default YoutubeNav;
